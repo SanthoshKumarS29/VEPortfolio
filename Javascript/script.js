0ce2a60d15abe162fb117c8fc3ef0830a8c71c5e
SanthoshKumarS29 <154403769+SanthoshKumarS29@users.noreply.github.com>
2024-03-11 18:24:00 +0530

    commit

---
+++ Javascript/script.js
@@ -0,0 +1,20 @@
+function menu() {
+    var x = document.getElementById("secondnav");
+  
+    if (x.style.display === "none" || x.style.display === "") {
+      x.style.display = "block";
+      
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function one() {
+    var x = document.getElementById("but");
+  
+    if (x.style.display === "none" || x.style.display === "") {
+      x.style.display = "block";
+      
+    } else {
+      x.style.display = "none";
+    }
+  }
